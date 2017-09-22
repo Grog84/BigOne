40ed626a3c436aaa786829700e8aec0ecf1d3ab3
Fabio Brigidi <fabio.brigidi@gmail.com>
2017-09-22 16:02:09 +0200

    Added more 3rd parties assets

---
+++ Assets/Prototype/Scripts/EnemyStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Prototype/Stats/Enemy")]
+public class EnemyStats : ScriptableObject {
+
+    // Steering
+    public float speed = 3.0f;
+    public float angularSpeed = 180f;
+    public float acceleration = 10f;
+    public float stoppingDistance = 2f;
+
+}

1815deba3bba167b5b0f676dfd293ed048106436
Fabio Brigidi <fabio.brigidi@gmail.com>
2017-09-22 18:23:40 +0200

    Started implementing a patrol AI on the enemies

---
+++ Assets/Prototype/Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Prototype/State")]
+public class State : ScriptableObject {
+
+    public AIAction[] actions;
+    public Color sceneGizmosColor = Color.gray;
+
+    // Every time we are calling this function we are evaluating all the actions and decisions connected to the state
+    public void UpdateState(StateController controller)
+    {
+        DoActions(controller);
+    }
+
+    private void DoActions(StateController controller)
+    {
+        for (int i = 0; i < actions.Length; i++)
+        {
+            actions[i].Execute(controller);
+        }
+    }
+	
+}
--- Assets/Prototype/Scripts/StateController.cs
+++ Assets/Prototype/Scripts/StateController.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class StateController : MonoBehaviour {
 
+    public State currentState;
+    public EnemyStats enemyStats;
+    public Transform eyes;
+    public List<Transform> wayPointList;
+
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+    [HideInInspector] public int nextWayPoint;
+
+    private bool isActive;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (!isActive)
+            return;
+        currentState.UpdateState(this);
+
 	}
+
+    private void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmosColor;
+            Gizmos.DrawSphere(eyes.position, 0.2f);
+        }
+    }
 }

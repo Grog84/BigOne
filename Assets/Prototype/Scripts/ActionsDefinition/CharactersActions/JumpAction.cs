8952bb4b6ca5620510ef7101c3eb0c4d467c0c36
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-09-28 07:55:26 +0200

    Created the scriptble object Jump definition for the min characters. Created the corresponding SO in the given folder

---
+++ Assets/Prototype/Scripts/ActionsDefinition/CharactersActions/JumpAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[CreateAssetMenu(menuName = "Prototype/CharactersActions/Jump")]
+public class JumpAction : _Action
+{
+    public override void Execute(StateController controller)
+    {
+        Jump(controller);
+    }
+
+    private void Jump(StateController controller)
+    {
+        bool m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (m_Jump)
+        {
+            // jump!
+            controller.characterObj.m_Rigidbody.velocity = new Vector3(controller.characterObj.m_Rigidbody.velocity.x,
+                controller.characterStats.m_JumpPower, controller.characterObj.m_Rigidbody.velocity.z);
+            controller.characterStats.m_GroundCheckDistance = 0.1f;
+        }
+    }
+}
--- Assets/Prototype/Scripts/CharacterObj.cs
+++ Assets/Prototype/Scripts/CharacterObj.cs
@@ -6,5 +6,5 @@ public class CharacterObj : ScriptableObject {
 
     public Transform m_Camera;                   // A reference to the main camera in the scenes transform
     public Transform CharacterTansform;       // A reference to the character assigned to the state controller transform
-
+    public Rigidbody m_Rigidbody;       // A reference to the character assigned to the state controller transform
 }

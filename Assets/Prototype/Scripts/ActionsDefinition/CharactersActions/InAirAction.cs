16b3393f0b946d6c437fe88a2966c3097ec16c02
Fabio Brigidi <fabio.brigidi@gmail.com>
2017-10-02 11:45:42 +0200

    Added script for the airborn movement and modified the state machine in order to include it.

---
+++ Assets/Prototype/Scripts/ActionsDefinition/CharactersActions/InAirAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Prototype/CharactersActions/AirbornMovement")]
+public class InAirAction : _Action {
+
+    public override void Execute(StateController controller)
+    {
+        AirbornMovement(controller);
+    }
+
+    private void AirbornMovement(StateController controller)
+    {
+        // apply extra gravity from multiplier:
+        Vector3 extraGravityForce = (Physics.gravity * controller.characterStats.m_GravityMultiplier) - Physics.gravity;
+        controller.characterObj.m_Rigidbody.AddForce(extraGravityForce);
+    }
+}
--- Assets/Prototype/Scripts/CharacterStats.cs
+++ Assets/Prototype/Scripts/CharacterStats.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CharacterStats : ScriptableObject { // nope...here stats only should be present, another SO should contain the gameobj refs
 
-    [SerializeField] public float m_MovingTurnSpeed = 360;
-    [SerializeField] public float m_StationaryTurnSpeed = 180;
-    [SerializeField] public float m_JumpPower = 12f;
-    [Range(1f, 4f)] [SerializeField] public float m_GravityMultiplier = 2f;
-    [SerializeField] public float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
-    [SerializeField] public float m_MoveSpeedMultiplier = 1f;
-    [SerializeField] public float m_AnimSpeedMultiplier = 1f;
-    [SerializeField] public float m_GroundCheckDistance = 0.1f;
+    public float m_MovingTurnSpeed = 360;
+    public float m_StationaryTurnSpeed = 180;
+    public float m_JumpPower = 12f;
+    [Range(1f, 4f)] public float m_GravityMultiplier = 2f;
+    public float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+    public float m_MoveSpeedMultiplier = 1f;
+    public float m_AnimSpeedMultiplier = 1f;
+    public float m_GroundCheckDistance = 0.1f;
 
 }

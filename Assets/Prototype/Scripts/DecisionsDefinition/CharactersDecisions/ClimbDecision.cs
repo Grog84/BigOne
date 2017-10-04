a29ec84dbea77ed0fb4f89d75d478d923c711fac
Andrea Voscilla <Andrea.voscilla@hotmail.it>
2017-10-04 17:32:47 +0200

    Climbing state created

---
+++ Assets/Prototype/Scripts/DecisionsDefinition/CharactersDecisions/ClimbDecision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Prototype/Decisions/Characters/ClimbDecision")]
+public class ClimbDecision : Decision
+{
+    public override bool Decide(CharacterStateController controller)
+    {
+        bool isClimbing = CheckIfClimbing(controller);
+        return isClimbing;
+    }
+
+    private bool CheckIfClimbing(CharacterStateController controller)
+    {
+        if()
+    }
+
+
+}

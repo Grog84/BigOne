a0a34d4057bd54164507b9bc4c23bdca32b63200
Andrea Tonnini <at.rsm@hotmail.it>
2017-10-10 17:28:37 +0200

    Started scripting a save status script, cloned final map designed and mapped for occlusion culling

---
+++ Assets/Prototype/Scripts/AT_provaScritp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AT_provaScritp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("CIAO HELLO WORLD");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

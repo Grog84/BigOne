7a54280b1ae2ae8d779c73950c59fe3ce1887a99
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-10-11 08:36:18 +0200

    Started implementing the perception cone of the guards

---
+++ Assets/Prototype/Scripts/Cone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cone : MonoBehaviour {
+
+    public ConeStats coneStats;
+    public bool isDebug;
+
+	void Start () {
+
+        transform.localScale.Set(coneStats.scaleX, coneStats.scaleY, coneStats.scaleZ);
+	}
+
+    private void Update()
+    {
+        if (isDebug)
+        {
+            transform.localScale.Set(coneStats.scaleX, coneStats.scaleY, coneStats.scaleZ);
+        }
+
+    }
+
+}
---
+++ Assets/Prototype/Scripts/ConeStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Prototype/ConeStats")]
+public class ConeStats : ScriptableObject {
+
+    [Range(0.5f, 10f)]
+    public float scaleX=1f;
+    [Range(0.5f, 10f)]
+    public float scaleY=1f;
+    [Range(0.5f, 10f)]
+    public float scaleZ=1f;
+
+}

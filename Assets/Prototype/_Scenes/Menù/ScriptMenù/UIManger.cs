be76827bee8644bcaba76ad7df4c583fe4276c58
Faku Gio <facundo.giovannone@studenti.unimi.it>
2017-10-18 21:30:04 +0200

    Added Substance Materials and added Materials to FG_MappaP_01

---
+++ Assets/Prototype/_Scenes/Menù/ScriptMenù/UIManger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManger : MonoBehaviour
+{
+   
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("FG_MappaP_01");
+    }
+    
+
+}

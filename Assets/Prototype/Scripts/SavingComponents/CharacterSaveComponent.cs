bbb69cf636eabcef00f8c0fd77aae8d6862dd8f9
FabioBrigidi <fabio.brigidi@gmail.com>
2017-10-20 11:56:15 +0200

    Modified Boy animator in order to make it recover from death animation

--- Assets/Prototype/Scripts/SavingComponents/CharacterSaveComponent.cs
+++ Assets/Prototype/Scripts/SavingComponents/CharacterSaveComponent.cs
@@ -5,47 +5,23 @@ using UnityEngine;
 public class CharacterSaveComponent : SaveObjComponent
 {
 
-    private EnemiesAIStateController m_Controller;
-
-    public enum GuardStates { Patrol, CheckPosition, Inactive }
-
-    [HideInInspector] public GuardStates activeState;
-
+    private CharacterStateController m_Controller;
 
     private void Awake()
     {
-        m_Controller = GetComponent<EnemiesAIStateController>();
+        m_Controller = GetComponent<CharacterStateController>();
     }
 
     public override void LoadData()
     {
         base.LoadData();
-        activeState = (GuardStates)PlayerPrefs.GetInt(saveObjName + "status");
-        switch (activeState)
-        {
-            case GuardStates.Patrol:
-                m_Controller.TransitionToState(m_Controller.patrolState);
-                break;
-            case GuardStates.CheckPosition:
-                m_Controller.m_AgentController.navPointTimer = PlayerPrefs.GetFloat(saveObjName + "navPointTimer");
-                m_Controller.TransitionToState(m_Controller.checkNavPoint);
-                break;
-            case GuardStates.Inactive:
-                m_Controller.TransitionToState(m_Controller.inactiveState);
-                break;
-            default:
-                break;
-        }
+        m_Controller.TransitionToState(m_Controller.gameStartState);
+
+
     }
 
     public override void SaveData()
     {
         base.SaveData();
-        PlayerPrefs.SetInt(saveObjName + "status", (int)activeState);
-
-        if (activeState == GuardStates.CheckPosition)
-        {
-            PlayerPrefs.SetFloat(saveObjName + "navPointTimer", m_Controller.m_AgentController.navPointTimer);
-        }
     }
 }

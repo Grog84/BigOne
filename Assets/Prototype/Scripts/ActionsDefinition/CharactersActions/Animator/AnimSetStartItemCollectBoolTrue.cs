7fb60c1fc25f60a5234ab78441363574b9322109
Galahad91 <alex.elia.91@gmail.com>
2017-10-20 18:05:52 +0200

    Added Animator states, scripts and animations for interaction with collectibles and doors

---
+++ Assets/Prototype/Scripts/ActionsDefinition/CharactersActions/Animator/AnimSetStartItemCollectBoolTrue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Prototype/CharactersActions/Animator/ActivateStartItemCollect")]
+public class AnimSetStartItemCollectBoolTrue : _Action
+{
+
+    public override void Execute(CharacterStateController controller)
+    {
+        UpdateAnimatorForStartItemCollect(controller);
+    }
+
+    private void UpdateAnimatorForStartItemCollect(CharacterStateController controller)
+    {
+        controller.m_CharacterController.m_Animator.SetBool("isCollecting", true);
+    }
+}

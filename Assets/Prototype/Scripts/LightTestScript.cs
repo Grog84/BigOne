bc4f3ef1dcd81d5e52c9eeb2843b7cb59200c25a
Andrea Tonnini <at.rsm@hotmail.it>
2017-10-24 15:29:46 +0200

    Shadow on Baked Lightning script added

--- Assets/Prototype/Scripts/LightTestScript.cs
+++ Assets/Prototype/Scripts/LightTestScript.cs
@@ -3,30 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightTestScript : MonoBehaviour {
-   /* public Light test;
+    private Light thisLight;
+    private Transform thisTransform;
+
+    public Transform player;
+
+    public Light worldLight;
+    public Transform worldLightTransform;    
+    // Update is called once per frame
+    private void Start()
+    {
+        thisLight = this.GetComponent<Light>();
+        thisTransform = this.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+       if (Vector3.Distance(player.position, thisTransform.transform.position) <= 10f)
+        {
+        worldLight.type=thisLight.type;
+        worldLightTransform.transform.position=thisTransform.transform.position;
+            worldLightTransform.position += Vector3.up;
+        }
+        else
+        {
+            worldLight.type = LightType.Directional;
+        }
+    }
+ #region LightFollowPalyer
+      /* public Light test;
     public Transform playerTransform;
 	// Use this for initialization
 	void Start () {
         test = GetComponent<Light>();
-	}*/
+	}
 
-    //values that will be set in the Inspector
+        //values that will be set in the Inspector
     public Transform Target;
     public float RotationSpeed;
-   
 
-   
+
+
 
     //values for internal use
     private Quaternion _lookRotation;
     private Vector3 _direction;
-
-    // Update is called once per frame
-    void Update()
+ void roba()
     {
-        if (Vector3.Distance(Target.position, this.transform.position) <= 10f)
-        {
-            GetComponent<Light>().intensity = 1;
+
+       
+    GetComponent<Light>().intensity = 1;
             //find the vector pointing from our position to the target
             _direction = (Target.position - transform.position).normalized;
 
@@ -35,11 +61,7 @@ public class LightTestScript : MonoBehaviour {
 
             //rotate us over time according to speed until we are in the required rotation
             transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
-        }
-        else
-        {
-            this.transform.eulerAngles = new Vector3(90, 0, 0);
-            GetComponent<Light>().intensity = 0;
-        }
-    }
+
+    }*/
+    #endregion
 }

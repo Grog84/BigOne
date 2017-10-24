cb0a08d52eb16b68c3a8de6eaa1b8929025377a4
Andrea Voscilla <Andrea.voscilla@hotmail.it>
2017-10-24 13:31:18 +0200

    Imported Cinemachine, and updated the groundCheckDecision

--- Assets/Prototype/Scripts/DecisionsDefinition/CharactersDecisions/GroundCheckDecision.cs
+++ Assets/Prototype/Scripts/DecisionsDefinition/CharactersDecisions/GroundCheckDecision.cs
@@ -1,40 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using StateMachine;
-
-namespace Character.Decisions
+namespace StateMachine
 {
+
+
     [CreateAssetMenu(menuName = "Prototype/Decisions/Characters/GroundCheck")]
     public class GroundCheckDecision : Decision
     {
-        public float offset = 0.1f;
+        public float verticalOffset = 0.5f;
+        public float verticalMainOffset = 0.1f;
+        public float horizontalOffset = 0.5f;
+        public int raycastCollided;
+        bool isOnTheGround;
+        public bool[] hits = new bool[5];
+        float secondaryRaycastDistance;
+
         public override bool Decide(CharacterStateController controller)
         {
-            bool isOnTheGround = CheckIsOnGround(controller);
-            return isOnTheGround;
-        }
-
-        private bool CheckIsOnGround(CharacterStateController controller)
-        {
-            RaycastHit hitInfo;
-#if UNITY_EDITOR
-            // helper to visualise the ground check ray in the scene view
-            Debug.DrawLine(controller.m_CharacterController.CharacterTansform.position,
-                            controller.m_CharacterController.CharacterTansform.position + (Vector3.down * controller.characterStats.m_GroundCheckDistance), Color.green);
-#endif
-            // ray_lenght is a small offset to start the ray from inside the character
-            // it is also good to note that the transform position in the sample assets is at the base of the character
-            // Debug.Log(Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up * offset, Vector3.down, out hitInfo, 0.15f));
-            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up * offset, Vector3.down, out hitInfo, controller.characterStats.m_GroundCheckDistance))
+            raycastCollided = CheckIsOnGround(controller);
+            if (raycastCollided > 0)
             {
-                //controller.m_CharacterController.floorNoiseMultiplier = hitInfo.transform.GetComponent<Floor>().GetNoiseMultiplier();
-                return true;
+                isOnTheGround = true;
             }
             else
             {
-                return false;
+                isOnTheGround = false;
             }
+
+
+            return isOnTheGround;
+        }
+
+        private int CheckIsOnGround(CharacterStateController controller)
+        {
+            secondaryRaycastDistance = controller.characterStats.m_GroundCheckDistance + 1 - verticalMainOffset;
+            RaycastHit hitInfo;
+
+
+#if UNITY_EDITOR
+            // helper to visualise the ground check ray in the scene view
+            Debug.DrawLine(controller.m_CharacterController.CharacterTansform.position + Vector3.up * verticalOffset,
+                            controller.m_CharacterController.CharacterTansform.position + (Vector3.down * controller.characterStats.m_GroundCheckDistance), Color.green);
+
+#endif
+            // creating an array of bool given by the raycasts to make the check more precise
+            // it is also good to note that the transform position in the sample assets is at the base of the character
+            // Debug.Log(Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up * offset, Vector3.down, out hitInfo, 0.15f));
+            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up * verticalMainOffset, Vector3.down, out hitInfo, controller.characterStats.m_GroundCheckDistance))
+            {
+                hits[0] = true;
+
+            }
+            else
+            {
+                hits[0] = false;
+            }
+
+            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up + Vector3.left * horizontalOffset, Vector3.down, out hitInfo, secondaryRaycastDistance))
+            {
+                hits[1] = true;
+            }
+            else
+            {
+                hits[1] = false;
+            }
+
+            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up + Vector3.forward * horizontalOffset, Vector3.down, out hitInfo, secondaryRaycastDistance))
+            {
+                hits[2] = true;
+            }
+            else
+            {
+                hits[2] = false;
+            }
+
+            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up + Vector3.right * horizontalOffset, Vector3.down, out hitInfo, secondaryRaycastDistance))
+            {
+                hits[3] = true;
+            }
+            else
+            {
+                hits[3] = false;
+            }
+
+            if (Physics.Raycast(controller.m_CharacterController.CharacterTansform.position + Vector3.up + Vector3.back * horizontalOffset, Vector3.down, out hitInfo, secondaryRaycastDistance))
+            {
+                hits[4] = true;
+            }
+            else
+            {
+                hits[4] = false;
+            }
+
+            int count = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i] == true)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
 

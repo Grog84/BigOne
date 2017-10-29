fc1fbf35ebbdb3c82e3094f424ecab98e03a4d3c
Andrea Voscila <Andrea.voscilla@hotmail.it>
2017-10-29 15:40:52 +0100

    imported playable assets and Playable wizard, created the Cutscene Manager and CutsceneTrigger scripts to manage the cutscenes throught various situations, added a florder to store various timelines

---
+++ Assets/Prototype/Scripts/CutsceneManager/CutsceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class CutsceneManager : MonoBehaviour
+{
+    
+    public PlayableDirector m_PlayableDirector;
+    public bool trigger = false;
+    public bool characterControlEnabled = false;
+
+    public void PlayTimeline(PlayableDirector playableDirector, bool triggered)
+    {
+        if (triggered == false)
+        {
+            playableDirector.Play();
+        }
+
+        trigger = true;
+    }
+	
+}
---
+++ Assets/Prototype/Scripts/CutsceneManager/CutsceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTrigger : CutsceneManager
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayTimeline(m_PlayableDirector,trigger);
+        }
+    }
+
+}

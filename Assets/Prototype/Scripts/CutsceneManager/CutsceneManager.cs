9414fa485491830efbe1696dbe5624ab25c313e5
Andrea Voscilla <Andrea.voscilla@hotmail.it>
2017-10-30 17:26:18 +0100

    Added cutsceneInteractable and cutsceneOnAwake scripts, implemented the possibility to lock the movement of the character during the cutscenes

--- Assets/Prototype/Scripts/CutsceneManager/CutsceneManager.cs
+++ Assets/Prototype/Scripts/CutsceneManager/CutsceneManager.cs
@@ -7,19 +7,47 @@ using UnityEngine.Playables;
 
 public class CutsceneManager : MonoBehaviour
 {
-    
+   
+    public TimelineClip m_Timeline;
     public PlayableDirector m_PlayableDirector;
     public bool trigger = false;
     public bool characterControlEnabled = false;
 
-    public void PlayTimeline(PlayableDirector playableDirector, bool triggered)
+
+   
+
+
+    public IEnumerator PlayTimeline(PlayableDirector playableDirector, bool triggered ,bool characterControllable)
     {
         if (triggered == false)
         {
             playableDirector.Play();
         }
-
         trigger = true;
+
+        if(characterControllable == false)
+        {
+            if(playableDirector.playableGraph.IsPlaying())
+            {
+                Debug.Log("INIZIATO");
+                GMController.instance.SetActive(false);
+            }
+
+            yield return new WaitForSeconds((float)playableDirector.duration);
+
+            if(playableDirector.state != PlayState.Playing)
+            {
+                GMController.instance.SetActive(true);
+            }
+        }
+
+
+
+        yield return null;
     }
 	
+    
+        
+   
+
 }
---
+++ Assets/Prototype/Scripts/CutsceneManager/CutsceneOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+using UnityEngine.Timeline;
+
+
+public class CutsceneOnAwake : CutsceneManager
+{
+
+    private void Awake()
+    {
+        m_PlayableDirector = this.GetComponent<PlayableDirector>();
+
+        StartCoroutine(PlayTimeline(m_PlayableDirector, trigger, characterControlEnabled));
+    }
+
+}

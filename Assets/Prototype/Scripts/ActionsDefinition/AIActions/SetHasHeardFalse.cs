72cd785873e65252520985ca8aa62c7cca588dc5
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-02 15:35:09 +0100

    Updated boy and mother stats in the prefab

---
+++ Assets/Prototype/Scripts/ActionsDefinition/AIActions/SetHasHeardFalse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+
+namespace AI.Actions
+{
+    [CreateAssetMenu(menuName = "Prototype/Actions/AI/Guard/SetHasHeardFalse")]
+    public class SetHasHeardFalse : _Action
+    {
+        public override void Execute(EnemiesAIStateController controller)
+        {
+            SetHeardFalse(controller);
+        }
+
+        private void SetHeardFalse(EnemiesAIStateController controller)
+        {
+            controller.m_AgentController.hasHeardPlayer = false;
+
+        }
+    }
+    
+}

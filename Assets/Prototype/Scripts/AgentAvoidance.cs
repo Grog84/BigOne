683976c68f7bf83cb129595c02bb061272d7c1a1
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-11-02 18:36:31 +0100

    Modified gurd prefab trying to solve the agents collision issue

---
+++ Assets/Prototype/Scripts/AgentAvoidance.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentAvoidance : MonoBehaviour {
+
+    [HideInInspector] public float myOrder = 0f;
+
+    [HideInInspector] public NavMeshAgent m_NavmeshAgent;
+    [HideInInspector] public NavMeshObstacle m_NavmeshObstacle;
+
+    private void Awake()
+    {
+        m_NavmeshAgent = GetComponent<NavMeshAgent>();
+        m_NavmeshObstacle = GetComponent<NavMeshObstacle>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            myOrder = Random.value;
+            var otherAgentAvoidance = other.gameObject.GetComponent<AgentAvoidance>();
+            if (myOrder < otherAgentAvoidance.myOrder)
+            {
+                m_NavmeshAgent.enabled = false;
+                m_NavmeshObstacle.enabled = true;
+
+                otherAgentAvoidance.m_NavmeshObstacle.enabled = false;
+                otherAgentAvoidance.m_NavmeshAgent.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            var otherAgentAvoidance = other.gameObject.GetComponent<AgentAvoidance>();
+            if (myOrder < otherAgentAvoidance.myOrder)
+            {
+                m_NavmeshObstacle.enabled = false;
+                m_NavmeshAgent.enabled = true;
+
+                otherAgentAvoidance.m_NavmeshObstacle.enabled = false;
+                otherAgentAvoidance.m_NavmeshAgent.enabled = true;
+            }
+        }
+    }
+}

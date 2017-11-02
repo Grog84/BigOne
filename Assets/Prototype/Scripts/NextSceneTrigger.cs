db349ef4ffe2b7dacc845385c30fcff2ec6b3600
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-02 13:45:57 +0100

    Created a trigger for the switch to next scene

---
+++ Assets/Prototype/Scripts/NextSceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextSceneTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCount > nextSceneIndex)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}

9b082f847cfcf5ecf8c040e342e0c62127ed9059
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-07 17:02:30 +0100

    Started implmenting the AI tool for the creation of the state machines

---
+++ Assets/Prototype/Scripts/Tools/ActionHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+using UnityEngine.UI;
+
+namespace AI.Tool
+{
+    public class ActionHolder : MonoBehaviour
+    {
+        public _Action m_Action;
+        public Text m_Text;
+
+        private void OnValidate()
+        {
+            m_Text.text = m_Action.name;
+        }
+
+    }  
+
+}
+
---
+++ Assets/Prototype/Scripts/Tools/DecisionHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+using UnityEngine.UI;
+
+namespace AI.Tool
+{
+    public class DecisionHolder : MonoBehaviour
+    {
+        public Decision m_Decision;
+        public Text m_Text;
+
+        private void OnValidate()
+        {
+            m_Text.text = m_Decision.name;
+        }
+
+    }  
+
+}
+

e56d25406724abab8bc0d07d6fa2771b68325104
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-08 17:10:31 +0100

    Modified Behaviour tree DM

--- Assets/Prototype/Scripts/AI/BehaviourTreeDM.cs
+++ Assets/Prototype/Scripts/AI/BehaviourTreeDM.cs
@@ -6,10 +6,43 @@ namespace AI.BT
 {
     public class BehaviourTreeDM : DecisionMaker
     {
+        // Blackboard Area
+        // ...
+
+        Task rootTask;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Build the tree from game objects
+
+            rootTask = GetComponentInChildren<Task>();
+            BuildTree(rootTask);
+        }
+
+        public void BuildTree(Task parentTask)
+        {
+            //parentTask.m_Agent = GetComponent<Agent>();
+            //parentTask.btdm = this;
+
+            if (parentTask.transform.childCount > 0)
+            {
+                // This is a composite task
+                Composite composite = parentTask as Composite;
+                composite.children = new List<Task>();
+
+                foreach (Transform child in composite.transform)
+                {
+                    Task childTask = child.GetComponent<Task>();
+                    composite.children.Add(childTask);
+                    BuildTree(childTask);
+                }
+            }
+        }
 
         public override void MakeDecision()
         {
-            
+            rootTask.Run();
         }
     }
 }

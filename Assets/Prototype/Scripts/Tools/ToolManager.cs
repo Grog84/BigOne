54a0252edf7f6b7663a29265733392676095c755
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-11-08 08:33:20 +0100

    Updated AITool

--- Assets/Prototype/Scripts/Tools/ToolManager.cs
+++ Assets/Prototype/Scripts/Tools/ToolManager.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using StateMachine;
+using System.IO;
 
 public class ToolManager : MonoBehaviour {
 
+    private List<_Action> allActions;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        allActions = new List<_Action>();
+
+        DirectoryInfo dir = new DirectoryInfo("Assets/Prototype/ScriptableObjects/Actions/Enemies");
+        FileInfo[] info = dir.GetFiles("*.asset");
+
+        foreach (FileInfo f in info)
+        {
+            //allActions.Add(AssetDatabase.FindAssets(f.FullName));
+        }
+
+        //allActions = AssetDatabase.FindAssets("t:ScriptObj", ["Assets/MyAwesomeProps"]);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {

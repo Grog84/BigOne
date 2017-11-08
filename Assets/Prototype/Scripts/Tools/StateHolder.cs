12d565a0422e48c793467202b24ded4a14a716fb
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-11-08 08:19:34 +0100

    Added component to state in AITool in order to load the proper SO. Started implementin gthe ToolManager

---
+++ Assets/Prototype/Scripts/Tools/StateHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+using UnityEngine.UI;
+
+namespace AI.Tool
+{
+    public class StateHolder : MonoBehaviour
+    {
+        public State m_State;
+        public Text m_Text;
+
+        private void OnValidate()
+        {
+            m_Text.text = m_State.name;
+        }
+
+    }  
+
+}
+

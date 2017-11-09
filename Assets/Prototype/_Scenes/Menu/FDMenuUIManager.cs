fcec85433404162890a54100a461b9b7702b7ed5
dbga_38 <dbga_38@dbga.local>
2017-11-09 10:51:48 +0100

    Edited my menu scene

---
+++ Assets/Prototype/_Scenes/Menu/FDMenuUIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FDMenuUIManager : MonoBehaviour 
+{
+	public Animator startButtonAnim;
+	public Animator settingsButtonAnim;
+	public Animator settingsPanelAnim;
+	public Animator slideMenuAnim;
+
+	public void StartGame () 
+	{
+		SceneManager.LoadScene ("FG_MappaP_01");
+	}
+
+	public void OpenSettings()
+	{
+		startButtonAnim.SetBool ("IsHidden", true);
+		settingsButtonAnim.SetBool ("isHidden", true);
+		settingsPanelAnim.SetBool ("IsHidden", false);
+	}
+
+	public void CloseSettings()
+	{
+		startButtonAnim.SetBool ("IsHidden", false);
+		settingsButtonAnim.SetBool ("isHidden", false);
+		settingsPanelAnim.SetBool ("IsHidden", true);
+	}
+
+	public void slideMenuUp()
+	{
+		bool isHidden = slideMenuAnim.GetBool ("isHidden");
+		slideMenuAnim.SetBool ("isHidden", !isHidden);
+	}
+
+}

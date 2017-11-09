9a1169361f07685cac655fe5f65de30eb9e2acd2
dbga_38 <dbga_38@dbga.local>
2017-11-09 13:22:31 +0100

    Edited myMenu

--- Assets/Prototype/_Scenes/Menu/FDMenuUIManager.cs
+++ Assets/Prototype/_Scenes/Menu/FDMenuUIManager.cs
@@ -8,32 +8,38 @@ public class FDMenuUIManager : MonoBehaviour
 {
 	public Animator startButtonAnim;
 	public Animator settingsButtonAnim;
-	public Animator settingsPanelAnim;
-	public Animator slideMenuAnim;
+	public Animator continueAnim;
+	public Animator exitGameAnim;
+	public Animator audioGameAnim;
+	public Animator videoGameAnim;
+	public Animator commandsGameAnim;
+	public Animator backGameAnim;
 
 	public void StartGame () 
 	{
 		SceneManager.LoadScene ("FG_MappaP_01");
+		Debug.Log ("CHIAMO SCENA DI FACUNDO");
 	}
 
 	public void OpenSettings()
 	{
-		startButtonAnim.SetBool ("IsHidden", true);
-		settingsButtonAnim.SetBool ("isHidden", true);
-		settingsPanelAnim.SetBool ("IsHidden", false);
+		settingsButtonAnim.SetBool ("IsHidden", true);
 	}
 
 	public void CloseSettings()
 	{
-		startButtonAnim.SetBool ("IsHidden", false);
-		settingsButtonAnim.SetBool ("isHidden", false);
-		settingsPanelAnim.SetBool ("IsHidden", true);
+		settingsButtonAnim.SetBool ("IsHidden", false);
 	}
 
-	public void slideMenuUp()
+	public void slideMenu()
 	{
-		bool isHidden = slideMenuAnim.GetBool ("isHidden");
-		slideMenuAnim.SetBool ("isHidden", !isHidden);
+		bool isHidden = exitGameAnim.GetBool ("IsHidden");
+		exitGameAnim.SetBool ("isHidden", !isHidden);
+	}
+
+	public void Continue()
+	{
+		
 	}
 
 }

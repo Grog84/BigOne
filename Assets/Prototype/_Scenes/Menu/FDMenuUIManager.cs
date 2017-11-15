6fb28761122da8ed7d864df3aea829ef177d5e90
DEVOTI-DESKTOP\fabio <fabio.devoti@gmail.com>
2017-11-15 21:08:15 +0100

    FD Menu added something

--- Assets/Prototype/_Scenes/Menu/FDMenuUIManager.cs
+++ Assets/Prototype/_Scenes/Menu/FDMenuUIManager.cs
@@ -4,58 +4,109 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class FDMenuUIManager : MonoBehaviour 
+public class FDMenuUIManager : MonoBehaviour
 {
-	public Animator startButtonAnim;
-	public Animator optionButtonAnim;
-	public Animator continueAnim;
-	public Animator exitGameAnim;
-	public Animator audioGameAnim;
-	public Animator videoGameAnim;
-	public Animator commandsGameAnim;
-	public Animator backMenuAnim;
-	public Animator audioMenu;
+    public Animator startButtonAnim;
+    public Animator optionButtonAnim;
+    public Animator continueAnim;
+    public Animator exitGameAnim;
+    public Animator audioGameAnim;
+    public Animator videoGameAnim;
+    public Animator commandsGameAnim;
+    public Animator backMenuAnim;
+    public Animator audioMenu;
+    public Animator videoMenu;
+    public Animator schemeControlls;
 
-	public void StartGame () 
-	{
-		Debug.Log ("CHIAMO SCENA DI FACUNDO");
-		SceneManager.LoadScene ("FG_MappaP_01");
-	}
+    public void StartGame()
+    {
+        Debug.Log("CHIAMO SCENA DI FACUNDO");
+        SceneManager.LoadScene("FG_MappaP_01");
+    }
 
-	public void OpenSettings()
-	{
-		startButtonAnim.SetBool ("IsHidden", true);
-		optionButtonAnim.SetBool ("IsHidden", true);
-		continueAnim.SetBool ("IsHidden", true);
-		exitGameAnim.SetBool ("IsHidden", true);
-		audioGameAnim.SetBool ("IsHidden", false);
-		videoGameAnim.SetBool ("IsHidden", false);
-		commandsGameAnim.SetBool ("IsHidden", false);
-		backMenuAnim.SetBool ("IsHidden", false);
-	}
+    public void OpenSettings()
+    {
+        startButtonAnim.SetBool("IsHidden", true);
+        optionButtonAnim.SetBool("IsHidden", true);
+        continueAnim.SetBool("IsHidden", true);
+        exitGameAnim.SetBool("IsHidden", true);
+        audioGameAnim.SetBool("IsHidden", false);
+        videoGameAnim.SetBool("IsHidden", false);
+        commandsGameAnim.SetBool("IsHidden", false);
+        backMenuAnim.SetBool("IsHidden", false);
+    }
 
-	public void CloseSettings()
-	{
-		startButtonAnim.SetBool ("IsHidden", false);
-		optionButtonAnim.SetBool ("IsHidden", false);
-		continueAnim.SetBool ("IsHidden", false);
-		exitGameAnim.SetBool ("IsHidden", false);
-		audioGameAnim.SetBool ("IsHidden", true);
-		videoGameAnim.SetBool ("IsHidden", true);
-		commandsGameAnim.SetBool ("IsHidden", true);
-		backMenuAnim.SetBool ("IsHidden", true);
-	}
+    public void CloseSettings()
+    {
+        startButtonAnim.SetBool("IsHidden", false);
+        optionButtonAnim.SetBool("IsHidden", false);
+        continueAnim.SetBool("IsHidden", false);
+        exitGameAnim.SetBool("IsHidden", false);
+        audioGameAnim.SetBool("IsHidden", true);
+        videoGameAnim.SetBool("IsHidden", true);
+        commandsGameAnim.SetBool("IsHidden", true);
+        backMenuAnim.SetBool("IsHidden", true);
+        audioMenu.SetBool("Audio", false);
+        videoMenu.SetBool("Video", false);
+        schemeControlls.SetBool("Input", false);
+    }
 
-	public void AudioMenuEnter()
-	{
-		Debug.Log ("CHIAMO SCENA DI FACUNDO");
-		audioMenu.SetBool ("Audio", true);
-	}
+    public void AudioMenuEnter()
+    {
+        if(videoMenu == true)
+            {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            videoMenu.SetBool("Video", false);
+        }
+        if (schemeControlls == true)
+        {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            schemeControlls.SetBool("Input", false);
+        }
+        Debug.Log("CHIAMO SCENA DI FACUNDO");
+        audioMenu.SetBool("Audio", true);
+    }
+
+    public void VideoMenuEnter()
+    {
+        if (audioMenu == true)
+        {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            audioMenu.SetBool("Audio", false);
+        }
+        if (schemeControlls == true)
+        {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            schemeControlls.SetBool("Input", false);
+        }
+        Debug.Log("CHIAMO SCENA DI FACUNDO");
+        videoMenu.SetBool("Video", true);
+    }
+
+    public void Controlls()
+    {
+        if (audioMenu == true)
+        {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            audioMenu.SetBool("Audio", false);
+        }
+        if (videoMenu == true)
+        {
+            Debug.Log("CHIAMO SCENA DI FACUNDO");
+            videoMenu.SetBool("Video", false);
+        }
+        Debug.Log("CHIAMO SCENA DI FACUNDO");
+        schemeControlls.SetBool("Input", true);
+    }
+
+
 
 	public void Continue()
 	{
 		
 	}
+
+
 
     public void Exit()
     {

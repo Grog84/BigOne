2765c685da5825294b61e74a6f0213eda7b32036
Andrea Voscilla <Andrea.voscilla@hotmail.it>
2017-11-16 14:16:26 +0100

    modified the prefab of the mainCameraSystem

---
+++ Assets/Prototype/Scripts/CameraScripts/LookAtMovements.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMovements : MonoBehaviour
+{
+    Transform myTransform;
+
+    private void Start()
+    {
+        myTransform = GetComponent<Transform>();
+
+    }
+
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Prototype/Scripts/ScriptAI_Da_Verificare/WorldSpaceCoordiinates.cs
+++ Assets/Prototype/Scripts/ScriptAI_Da_Verificare/WorldSpaceCoordiinates.cs
@@ -7,6 +7,6 @@ public class WorldSpaceCoordiinates : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Debug.Log(this.transform.localToWorldMatrix.ToString());
 	}
 }

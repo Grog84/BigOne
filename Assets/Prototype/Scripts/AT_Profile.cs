a80c13e0ee3015e6f60d537fa5188930bb731304
Andrea Tonnini <at.rsm@hotmail.it>
2017-11-16 16:17:30 +0100

    Started prototyping Loading scene with locked and unlocked scene with last scene seen

---
+++ Assets/Prototype/Scripts/AT_Profile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AT_Profile 
+{
+    public string idProfile;
+    public bool[] completedLevel;
+    public int currentLevelIndex;
+
+    public AT_Profile(string _idProfile)
+    {
+        idProfile = _idProfile;
+
+    }
+}

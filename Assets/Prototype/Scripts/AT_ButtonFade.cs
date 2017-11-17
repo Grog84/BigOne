7e208ff77485b48c5a0c5c3c2c31faf851bbde7d
Andrea Tonnini <at.rsm@hotmail.it>
2017-11-17 12:13:38 +0100

    Implemented New Button Fade Script only on AT_ProvaAsyncLoad

---
+++ Assets/Prototype/Scripts/AT_ButtonFade.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class AT_ButtonFade:MonoBehaviour
+    {
+    [SerializeField] public Button[] SceneStatus;
+    private Image[] ImageStatus;
+    private void Start()
+    {
+        for (int i = 0; i < SceneStatus.Length; i++)
+        {
+            ImageStatus[i] = SceneStatus[i].gameObject.GetComponent<Image>();
+        }
+    }
+
+
+    public void FadeOut()
+    {
+
+        StartCoroutine(fadeOut());
+
+    }
+    public void FadeIn()
+    {
+        StartCoroutine(fadeIn());
+
+    }
+    IEnumerator fadeOut()
+    {
+        foreach (Image yourNameHere in ImageStatus)
+        {
+            while (yourNameHere.color.a > 0)
+            {                   //use "< 1" when fading in
+                yourNameHere.color = new Color(yourNameHere.color.r, yourNameHere.color.g, yourNameHere.color.b, yourNameHere.color.a - Time.deltaTime / 1);    //fades out over 1 second. change to += to fade in  
+                yourNameHere.transform.GetChild(0).GetComponent<Text>().color = new Color(yourNameHere.transform.GetChild(0).GetComponent<Text>().color.r,
+                    yourNameHere.transform.GetChild(0).GetComponent<Text>().color.g, yourNameHere.transform.GetChild(0).GetComponent<Text>().color.b,
+                    yourNameHere.transform.GetChild(0).GetComponent<Text>().color.a - Time.deltaTime / 1);
+                yield return null;
+            }
+            if (yourNameHere.color.a < 0)
+            {
+                yourNameHere.gameObject.SetActive(false);
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator fadeIn()
+    {
+        foreach (Image yourNameHere in ImageStatus)
+        {
+            yourNameHere.gameObject.SetActive(true);
+
+
+            while (yourNameHere.color.a < 1)
+            {                   //use "< 1" when fading in
+                yourNameHere.color = new Color(yourNameHere.color.r, yourNameHere.color.g, yourNameHere.color.b, yourNameHere.color.a + Time.deltaTime / 1);    //fades out over 1 second. change to += to fade in  
+                yourNameHere.transform.GetChild(0).GetComponent<Text>().color = new Color(yourNameHere.transform.GetChild(0).GetComponent<Text>().color.r,
+                    yourNameHere.transform.GetChild(0).GetComponent<Text>().color.g, yourNameHere.transform.GetChild(0).GetComponent<Text>().color.b,
+                    yourNameHere.transform.GetChild(0).GetComponent<Text>().color.a + Time.deltaTime / 1);
+                yield return null;
+            }
+        }
+    }
+}
--- Assets/Prototype/Scripts/AT_LoadSceneASync.cs
+++ Assets/Prototype/Scripts/AT_LoadSceneASync.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+
 public class AT_LoadSceneASync : MonoBehaviour {
    
     AsyncOperation ao;
@@ -15,13 +17,13 @@ public class AT_LoadSceneASync : MonoBehaviour {
     public RectTransform me;
 
     [SerializeField] public Button[] SceneStatus;
-
+  
     private void Start()
     {
         InizializeLevelButton();
+     
     }
-
-
+    //   public Image yourNameHere;
     public void StartLoad(int index)
     {
         _index = index;
@@ -155,4 +157,54 @@ public class AT_LoadSceneASync : MonoBehaviour {
 
     }
 
+    #region FadeButton
+    //public void FadeOut()
+    //{
+
+    //    StartCoroutine(fadeOut());
+
+    //}
+    //public void fadeIn()
+    //{
+    //    StartCoroutine(FadeIn());
+
+    //}
+    //IEnumerator fadeOut()
+    //{
+    //    foreach (Image yourNameHere in ImageStatus)
+    //    {
+    //        while (yourNameHere.color.a > 0)
+    //        {                   //use "< 1" when fading in
+    //            yourNameHere.color = new Color(yourNameHere.color.r, yourNameHere.color.g, yourNameHere.color.b, yourNameHere.color.a - Time.deltaTime / 1);    //fades out over 1 second. change to += to fade in  
+    //            yourNameHere.transform.GetChild(0).GetComponent<Text>().color = new Color(yourNameHere.transform.GetChild(0).GetComponent<Text>().color.r,
+    //                yourNameHere.transform.GetChild(0).GetComponent<Text>().color.g, yourNameHere.transform.GetChild(0).GetComponent<Text>().color.b,
+    //                yourNameHere.transform.GetChild(0).GetComponent<Text>().color.a - Time.deltaTime / 1);
+    //            yield return null;
+    //        }
+    //        if (yourNameHere.color.a < 0)
+    //        {
+    //            yourNameHere.gameObject.SetActive(false);
+    //            yield return null;
+    //        }
+    //    }
+    //}
+
+    //IEnumerator FadeIn()
+    //{
+    //    foreach (Image yourNameHere in ImageStatus)
+    //    {
+    //        yourNameHere.gameObject.SetActive(true);
+
+
+    //    while (yourNameHere.color.a <1)
+    //    {                   //use "< 1" when fading in
+    //        yourNameHere.color = new Color(yourNameHere.color.r, yourNameHere.color.g, yourNameHere.color.b, yourNameHere.color.a +Time.deltaTime / 1);    //fades out over 1 second. change to += to fade in  
+    //        yourNameHere.transform.GetChild(0).GetComponent<Text>().color = new Color(yourNameHere.transform.GetChild(0).GetComponent<Text>().color.r,
+    //            yourNameHere.transform.GetChild(0).GetComponent<Text>().color.g, yourNameHere.transform.GetChild(0).GetComponent<Text>().color.b,
+    //            yourNameHere.transform.GetChild(0).GetComponent<Text>().color.a + Time.deltaTime / 1);
+    //        yield return null;
+    //    }
+    //    }
+    //}
+#endregion
 }

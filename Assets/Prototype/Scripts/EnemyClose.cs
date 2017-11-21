f98df2ac553f94dbd34ec62d16fde0bf03dd6aa0
Alessandro Elia <Alex.elia.91@gmail.com>
2017-11-21 16:53:01 +0100

    Started new enemy radar system, added new prefabs and scripts

---
+++ Assets/Prototype/Scripts/EnemyClose.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClose : MonoBehaviour
+{
+    public GameObject enemyPointer;
+    public float pos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            pos -= 0.2f;
+            var thisPointer = Instantiate(enemyPointer, transform.parent);
+            thisPointer.GetComponent<EnemyRadar>().target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            pos += 0.2f;
+        }
+    }
+}
---
+++ Assets/Prototype/Scripts/RadarDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarDestroy : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == transform.parent.GetComponent<EnemyRadar>().target)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}

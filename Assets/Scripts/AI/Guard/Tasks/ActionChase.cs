46fdcedc19fa4913c7421915a0f69fbd121219ee
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-11-21 08:41:06 +0100

    Started implementing Chase action

---
+++ Assets/Scripts/AI/Guard/Tasks/ActionChase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI.BT
+{
+    public class ActionChase : Task
+    {
+        public override TaskState Run()
+        {
+            //m_BehaviourTree.m_Blackboard.m_NavMeshAgent.destination = controller.m_AgentController.chaseTarget.position;
+            //controller.m_AgentController.m_NavMeshAgent.isStopped = false;
+
+            return TaskState.SUCCESS;;
+        }
+    }
+}

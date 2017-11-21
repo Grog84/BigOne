3d95d24561b760047ad0e20d3f75adc3aea8e79e
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-21 17:27:08 +0100

    Started implemtnting selector tool

---
+++ Assets/Scripts/Tools/BTDMTool/SelectorTool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorTool : MonoBehaviour {
+
+    public GameObject selector;
+    public GameObject sequencer;
+    public GameObject task;
+
+    public void CreateSelector()
+    {
+        var thistask = Instantiate(selector, transform);
+        task.transform.position += Vector3.down;    
+    }
+
+    public void CreateSequencer()
+    {
+        var thistask = Instantiate(sequencer, transform);
+        task.transform.position += Vector3.down;
+    }
+
+    public void CreateTask()
+    {
+        var thistask = Instantiate(task, transform);
+        task.transform.position += Vector3.down;
+    }
+}

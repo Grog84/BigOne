9a8bf6771f4ef3aa56cc68fb638cacf329fd9e13
FabioBrigidi <fabio.brigidi@gmail.com>
2017-11-30 13:20:13 +0100

    Started implementing the guard tool for the creation of the patrol path

---
+++ Assets/Scripts/AI/Guard/Editor/NavPointCreatorEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NavPointCreator))]
+public class NavPointCreatorEditor : Editor
+{
+    NavPointCreator m_Target;
+
+    public override void OnInspectorGUI()
+    {
+        m_Target = (NavPointCreator)target;
+        DrawDefaultInspector();
+        DrawAddResetButtons();
+
+    }
+
+    void DrawAddResetButtons()
+    {
+        GUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("ADD"))
+            {
+                Debug.Log("add");
+                //m_Target.SaveConfig();
+                //EditorUtility.SetDirty(m_Target.myConfigFile);
+
+            }
+
+            if (GUILayout.Button("RESET"))
+            {
+                EditorApplication.Beep();
+                if (EditorUtility.DisplayDialog("Reset Navpoints", "Reset all the guard navpoints?", "Yes", "No"))
+                {
+                    Debug.Log("reset");
+                    //m_Target.LoadConfig();
+                }
+            }
+        }
+        GUILayout.EndHorizontal();
+    }
+}

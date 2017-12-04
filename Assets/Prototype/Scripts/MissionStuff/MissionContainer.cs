eed440d6d6802908db735c37c16e35167b6abd17
Andrea Tonnini <at.rsm@hotmail.it>
2017-12-04 16:15:39 +0100

    New Mission Folder, with Mission Manager Prefab to put in the Scene in order to start create new Mission

---
+++ Assets/Prototype/Scripts/MissionStuff/MissionContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System.Collections.Generic;
+
+
+namespace MissionManagerStuff
+{
+    [Serializable]
+    [ShowOdinSerializedPropertiesInInspector]
+   
+    public class MissionContainer
+    {
+
+   
+        public List<Mission> MissionList= new List<Mission>();
+
+    }
+}
+

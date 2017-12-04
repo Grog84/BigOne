9fdc0a90e32f0239179d8bb5912efa3a7e43bd68
FabioBrigidi <fabio.brigidi@gmail.com>
2017-12-04 15:50:43 +0100

    Implemented the Nav point instancing tool.

--- Assets/Scripts/AI/Guard/NavPointCreator.cs
+++ Assets/Scripts/AI/Guard/NavPointCreator.cs
@@ -6,10 +6,11 @@ using AI;
 public class NavPointCreator : MonoBehaviour
 {
     public Guard m_Guard;
+    public GameObject navPoint;
 
     public void AddNavpoint(NavPoint nvPoint)
     {
         m_Guard.wayPointList.Add(nvPoint);
-    }
-	
+    }	
+
 }

3deac259e859981f4ebfeed21eaa1da988ff6fe9
Andrea Voscilla <andrea.voscilla@hotmail.it>
2017-12-08 17:31:56 +0100

    updated the prefab of the questnpc, completed all the actinos of the npc except of the quest complete condition. created the behaviour tree of the questnpc.

--- Assets/Scripts/AI/NpcQuest/QuestNpc.cs
+++ Assets/Scripts/AI/NpcQuest/QuestNpc.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.AI;
 using SaveGame;
 using RootMotion.FinalIK;
+using MissionManagerStuff;
+using UnityEngine.Playables;
 
 namespace AI
 {
     public class QuestNpc : AIAgent
     {
+        QuestGiver m_QuestGiver;
+        public PlayableDirector m_PlayableDirector;
+
         LookAtIK lookAtComponent;
         public Transform lookAtTarget;
         [HideInInspector] public float headClamp;    // Reference to the maximum weight according to inspector values
@@ -54,6 +59,26 @@ namespace AI
             throw new System.NotImplementedException();
         }
 
+        public void SetQuestAvailable()
+        {
+            SetBlackboardValue("questAvailable", m_QuestGiver.myMission.available);
+        }
+
+        public void SetQuestCompleted()
+        {
+            SetBlackboardValue("questCompleted", m_QuestGiver.myMission.completed);
+        }
+            
+        public bool GetQuestAvailable()
+        {
+            return GetBlackboardBoolValue("questAvailable");
+        }
+
+        public bool GetQuestCompleted()
+        {
+            return GetBlackboardBoolValue("questCompleted");
+        }
+
         public void LookAtManager()
         {
             if(GetBlackboardBoolValue("lookAtPlayer"))
@@ -78,15 +103,20 @@ namespace AI
             m_Brain.decisionMaker.m_Blackboard = new QuestNpcBlackboard();
             m_Blackboard = m_Brain.decisionMaker.m_Blackboard;
             m_Blackboard.m_Agent = this;
+            m_QuestGiver = GetComponent<QuestGiver>();
         }
 
         private void Start()
         {
+            SetQuestAvailable();
+            SetQuestCompleted();
             headClamp = lookAtComponent.solver.headWeight;
         }
 
         private void Update()
         {
+            m_QuestGiver.myMission.available = GetQuestAvailable();
+            m_QuestGiver.myMission.completed = GetQuestCompleted();
             LookAtManager();
         }
     }

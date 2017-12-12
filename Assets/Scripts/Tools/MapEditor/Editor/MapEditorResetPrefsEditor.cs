2f42d93489037720987a6fcd809b120c169ea066
dbga_38 <dbga_38@dbga.local>
2017-12-12 16:14:52 +0100

    [Fabio Brigidi] Created a tool that allows a reset of the editor prefs in case the registered value will overflow after a modification of the assets database. Created a prefab containing the component that can be used when needed

---
+++ Assets/Scripts/Tools/MapEditor/Editor/MapEditorResetPrefsEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapEditorResetPrefs))]
+public class MapEditorResetPrefsEditor : Editor {
+
+	MapEditorResetPrefs m_Target;
+
+	public override void OnInspectorGUI()
+	{
+		m_Target = (MapEditorResetPrefs)target;
+		MapEditorResetPrefsButton ();
+
+	}
+
+	void MapEditorResetPrefsButton()
+	{
+		
+		if (GUILayout.Button("RESET"))
+		{
+			EditorApplication.Beep();
+			if (EditorUtility.DisplayDialog("Reset Prefs", "Are you sure you really want to reset the current Editor prefs?", "Yes", "No"))
+			{
+				m_Target.ResetPrefs();
+
+			}
+
+		}
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/Tools/MapEditor/MapEditorResetPrefs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapEditorResetPrefs : MonoBehaviour {
+
+	public void ResetPrefs()
+	{
+		EditorPrefs.SetBool("IsLevelEditorEnabled", false);
+		EditorPrefs.SetInt("SelectedBlock", 0);
+		EditorPrefs.SetInt("SelectedPrefab", 0);
+		EditorPrefs.SetInt("SelectedMaterial", 0);
+	}
+}

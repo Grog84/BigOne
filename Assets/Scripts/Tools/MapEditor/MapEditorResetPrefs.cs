85ea6acc0257ba4e7b727a022feb3ce2e85922c0
Fabio Brigidi <fabio.brigidi@gmail.com>
2017-12-15 16:32:16 +0100

    Fixed a bug that would prevent the project to be built because of a unityeditor import

--- Assets/Scripts/Tools/MapEditor/MapEditorResetPrefs.cs
+++ Assets/Scripts/Tools/MapEditor/MapEditorResetPrefs.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -13,3 +14,4 @@ public class MapEditorResetPrefs : MonoBehaviour {
 		EditorPrefs.SetInt("SelectedMaterial", 0);
 	}
 }
+#endif

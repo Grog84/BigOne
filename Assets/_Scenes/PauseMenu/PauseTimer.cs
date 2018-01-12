7cdf5f3ba37b5b483b2df2dd635b03b155f9e942
dbga_38 <dbga_38@dbga.local>
2018-01-12 13:34:32 +0100

    Pause Menu Edited

---
+++ Assets/_Scenes/PauseMenu/PauseTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseTimer : MonoBehaviour {
+	public float time;
+
+	public void WaitFor(){
+		
+	}
+
+	public void LoadScene(){
+		
+	}
+
+}

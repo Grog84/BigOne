c1c2e110c26c9b433f9616f9f957afaa528c421a
DESKTOP-BL268BR\fabio <fabio.devoti@gmail.com>
2018-01-14 10:18:25 +0100

    Pause Menu Edited

--- Assets/_Scenes/PauseMenu/PauseMenuUIManager.cs
+++ Assets/_Scenes/PauseMenu/PauseMenuUIManager.cs
@@ -13,13 +13,12 @@ public class PauseMenuUIManager : MonoBehaviour
 	public GameObject controlsImage; // Controls Image
 	public GameObject backToMainMenu; // Back to main menu button
 	public GameObject firstSelectedPauseMenuButton; // First button selected in the Main Menu
-	public GameObject firstSelectedbackToMainMenu; // First button selected in the Back to game choice
+	public GameObject firstSelectedBackToMainMenu; // First button selected in the Back to game choice
     public GameObject diaryPanel; // Diary panel
 	public GameObject diaryButton; // Diary Button
     public Button backToMainMenuYesButton; // The "Yes Button" in the "Security Menu"
     public Button backToMainMenuNoButton; // The "No Button" in the "Security Menu"
 	public EventSystem eventSystem;
-	public int variabilePauseMenu; // Variable that allow to the "AreYouSureMenu" what button has activated him;
 
     void Start() //Disable all the GameObject-Menu that has not to be on the screen
     {
@@ -43,53 +42,38 @@ public class PauseMenuUIManager : MonoBehaviour
     public void Controls() // Show the controls image
     {
         controlsImage.gameObject.SetActive(true); // Activate the controls image
-		backToMainMenuYesButton.gameObject.SetActive(false);
-		backToMainMenuNoButton.gameObject.SetActive(false);
-		diaryPanel.gameObject.SetActive(false);
-		variabilePauseMenu = 1;
+        diaryPanel.gameObject.SetActive(false);
+        backToMainMenuYesButton.gameObject.SetActive(false);
+        backToMainMenuNoButton.gameObject.SetActive(false);
     }
 
 	public void FadesPauseMenu(string pauseMenuType) // Set the time to wait until the fade animation is finished
 	{
 		Invoke (pauseMenuType, 0.2f); // Enable the Fade 
-		//switch (variabilePauseMenu) 
-		/*{
-		case 1: // Enable the Fade when the user press the "No Button" in the "AreYouSureMenu" and let him go back to the "Main Menu"
-			Invoke("MainMenu", 0.2f);
-			break;
-		case 2:
-			Invoke("SelectLevelMenu", 0.2f); // Enable the Fade when the user press the "No Button" in the "AreYouSureMenu" and let him go back to the "Select Level Menu"
-			break;
-		case 0:
-			
-			break;
-		default:
-			break;
-		}*/
 	}
 
 	public void DiaryPanel()
 	{
 		controlsImage.gameObject.SetActive(false); // Activate the diary panel
-		backToMainMenuYesButton.gameObject.SetActive(false);
-		backToMainMenuNoButton.gameObject.SetActive(false);
-		diaryPanel.gameObject.SetActive(true);
-		variabilePauseMenu = 2;
-	}
+        diaryPanel.gameObject.SetActive(true);
+        backToMainMenuYesButton.gameObject.SetActive(false);
+        backToMainMenuNoButton.gameObject.SetActive(false);
+    }
 
 	public void BackToMainMenuChoice()// Enable the choice to go the Main Menu
     {
-		eventSystem.SetSelectedGameObject(firstSelectedbackToMainMenu);
-		backToMainMenuYesButton.gameObject.SetActive(true);
+		eventSystem.SetSelectedGameObject(firstSelectedBackToMainMenu);
+        backToMainMenuYesButton.gameObject.SetActive(true);
         backToMainMenuNoButton.gameObject.SetActive(true);
     }
 
     public void BackToMainMenuNO()// Close the choice to go the Main Menu
     {
 		eventSystem.SetSelectedGameObject(firstSelectedPauseMenuButton);
-		backToMainMenuYesButton.gameObject.SetActive(false);
+        backToMainMenuYesButton.gameObject.SetActive(false);
         backToMainMenuNoButton.gameObject.SetActive(false);
-		variabilePauseMenu = 3;
+        controlsImage.gameObject.SetActive(false);
+        diaryPanel.gameObject.SetActive(true);
     }
 
     public void BackToMainMenuYes()// Go to the Main Menu

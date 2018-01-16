626aa54c704df3545e71908dd1ad891058e2f905
FabioBrigidi <fabio.brigidi@gmail.com>
2018-01-16 14:55:56 +0100

    Added new bank. Added framerate index in the asset test scene

---
+++ Assets/Scripts/Tools/Framerate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Framerate : MonoBehaviour {
+
+    Text m_text;
+    int frameRate;
+
+    void Start()
+    {
+        m_text = GetComponent<Text>();
+         
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        frameRate = (int)(1f / Time.deltaTime);
+        m_text.text = frameRate.ToString();
+
+    }
+}

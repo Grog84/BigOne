20599d143620c5ab53774703607d08b6868f8057
Alessandro Elia <Alex.elia.91@gmail.com>
2018-01-18 17:14:14 +0100

    Created Particle database and singleton script

---
+++ Assets/Prototype/Scripts/ParticleDB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+
+[CreateAssetMenu(menuName = "ParicleDB")]
+[Serializable]
+public class ParticleDB: ScriptableObject
+{
+    [BoxGroup("ParticleDB")]
+    [TableMatrix(HorizontalTitle = "Texture", VerticalTitle = "State")]
+    //public GameObject[,] LabledTable = new GameObject[15, 10];
+    public ParticleSystem[,] MyDB = new ParticleSystem[10,3];
+	
+}
---
+++ Assets/Scripts/ParticleSystemManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemManager : MonoBehaviour
+{
+    public static ParticleSystemManager instance = null;
+    public ParticleDB particleData;
+    Transform player;
+
+    void Awake()
+    {
+        //Singleton
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        player = GMController.instance.m_CharacterInterfaces[(int)GMController.instance.isCharacterPlaying].transform;
+    }
+
+    public void EmitRightParticle(int texture, int state, Vector3 textureNormal)
+    {
+        particleData.MyDB[texture, state].transform.position = player.position;
+        particleData.MyDB[texture, state].transform.rotation = Quaternion.LookRotation(Vector3.up);
+        particleData.MyDB[texture, state].Emit(1);
+    }
+}

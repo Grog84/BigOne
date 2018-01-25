a145d2df63c6f53d0fb208e6de90ae09485c984a
Andrea Tonnini <at.rsm@hotmail.it>
2018-01-25 16:39:22 +0100

    Fixed Canvas puase

--- Assets/_Scenes/PauseMenu/PauseMenuButtonUI.cs
+++ Assets/_Scenes/PauseMenu/PauseMenuButtonUI.cs
@@ -6,23 +6,4 @@ using UnityEngine.Experimental.UIElements;
 public class PauseMenuButtonUI : MonoBehaviour
 {
 
-    public PauseMenuUIManager m_Manager;
-    //BOOL ISACTIVE
-    public Button[] LevelSelect;
-
-    private void Start()
-    {
-        m_Manager = FindObjectOfType<PauseMenuUIManager>();
-        m_Manager.GetComponent<PauseMenuUIManager>();
-
-    }
-
-    //ACTIVATE
-    //DEACTIVATE
-
-
-    public void SelectButton()
-    {
-        m_Manager.eventSystem.SetSelectedGameObject(gameObject);
-    }
 }

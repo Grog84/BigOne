75e8a4bfa07d89815044745d71484d4b3ef3b54b
Fabio Brigidi <fabio.brigidi@gmal.com>
2018-01-25 08:31:27 +0100

    Created pedestrian animation controller script

---
+++ Assets/Scripts/AI/Pedestrian/PedestrianAnimController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+
+    public class PedestrianAnimController : MonoBehaviour
+    {
+
+        Pedestrian m_Pedestrian;
+        public float pedestrianAnimTick = 2f;
+        public float pedestrianClimbAnimation = 5f;
+
+        private void Awake()
+        {
+            m_Pedestrian = GetComponent<Pedestrian>();
+        }
+
+        IEnumerator AnimCO()
+        {
+            while (true)
+            {
+                m_Pedestrian.m_Animator.SetBool("Climbing", m_Pedestrian.m_Blackboard.GetBoolValue("PlayerIsClimbing"));
+                if(m_Pedestrian.m_Blackboard.GetBoolValue("PlayerIsClimbing"))
+                    yield return new WaitForSeconds(pedestrianClimbAnimation);
+                else
+                    yield return new WaitForSeconds(pedestrianAnimTick);
+            }
+        }
+    }
+}

4e9cda840a07de21a2c142bf9d48ad69ea13d9da
Alessandro Elia <Alex.elia.91@gmail.com>
2018-02-01 15:07:07 +0100

    Added New Animation for Mother created new state

---
+++ Assets/Prototype/Scripts/ActionsDefinition/CharactersActions/StartCarrying.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+
+namespace Character.Actions
+{
+    [CreateAssetMenu(menuName = "Prototype/Actions/Characters/StartCarrying")] 
+    public class CrouchOnStairs : _Action
+    {
+        public override void Execute(CharacterStateController controller)
+        {
+            StartCarrying(controller);
+        }
+
+        public void StartCarrying(CharacterStateController controller)
+        {
+            controller.characterStats.m_MovementSpeed = controller.characterStats.m_CrouchOnStarisSpeed;
+            controller.m_CharacterController.m_Animator.SetBool("isCarryingItem", true);
+        }
+
+    }
+}

f7ccbcd0241f8decfe901a4a3e5579cd8cf86cf9
Alessandro Elia <Alex.elia.91@gmail.com>
2018-02-13 15:14:10 +0100

    Added Events for sound play on animations

--- Assets/Scripts/PlayFMODOneShot.cs
+++ Assets/Scripts/PlayFMODOneShot.cs
@@ -2,21 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayFMODOneShot : MonoBehaviour {
+public class PlayFMODOneShot : MonoBehaviour
+{
 
     [FMODUnity.EventRef]
     public string m_ClimbPath;
 
     [FMODUnity.EventRef]
-    public string m_ClimbPath2;
+    public string m_PickItem;
+
+    [FMODUnity.EventRef]
+    public string m_OpenDoor;
+
+    [FMODUnity.EventRef]
+    public string m_LockedDoor;
+
+    [FMODUnity.EventRef]
+    public string m_Push;
+
+    [FMODUnity.EventRef]
+    public string m_Pull;
 
     string[] allSounds;
 
     private void Awake()
     {
-        allSounds = new string[2];
+        allSounds = new string[6];
         allSounds[0] = m_ClimbPath;
-        allSounds[1] = m_ClimbPath2;
+        allSounds[1] = m_PickItem;
+        allSounds[2] = m_OpenDoor;
+        allSounds[3] = m_LockedDoor;
+        allSounds[4] = m_Push;
+        allSounds[5] = m_Pull;
     }
 
     public void PlaySound(int soundIdx)

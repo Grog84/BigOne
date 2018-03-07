1d4299623c951b1b51d908e9c5bd03203f6a6947
FabioBrigidi <fabio.brigidi@gmail.com>
2018-03-07 15:04:36 +0100

    Fixed switch off sound clip

--- Assets/3rd Parties/DefaultPlayables/SwitchOffBkgMusic/SwitchOffBkgTrack.cs
+++ Assets/3rd Parties/DefaultPlayables/SwitchOffBkgMusic/SwitchOffBkgTrack.cs
@@ -4,7 +4,7 @@ using UnityEngine.Timeline;
 using UnityEngine.UI;
 
 [TrackColor(0.855f, 0.8623f, 0.87f)]
-[TrackClipType(typeof(SwitchSceneClip))]
+[TrackClipType(typeof(SwitchOffBkgClip))]
 public class SwitchOffBkgTrack : TrackAsset
 {
     public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)

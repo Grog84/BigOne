fb1f7f145f6afcbc1145bd3e8a2170d9678cb495
Manfredo Stagno <dbga_34@WKST_34.dbga.local>
2018-03-29 18:29:22 +0200

    New Test Scene for lighting + new script

---
+++ Assets/_Scenes/Beta/Livelli_Beta/Prefabs_Env/InputCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCheck : MonoBehaviour {
+
+
+	public GameObject keyboardIcon;
+	public GameObject joystickIcon;
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		if(InputManager.instance.GetInputState() == InputManager.InputState.MouseKeyboard)
+		{
+			keyboardIcon.SetActive () = true;
+			joystickIcon.SetActive () = false;
+		}
+		if(InputManager.instance.GetInputState() == InputManager.InputState.Controller)
+		{
+			keyboardIcon.SetActive () = false;
+			joystickIcon.SetActive () = true;
+		}
+	}
+}

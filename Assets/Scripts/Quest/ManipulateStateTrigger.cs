56a933d30a1b61081fb789a55b430f736a660d18
Andrea Voscilla <andrea.voscilla@hotmail.it>
2018-03-30 12:00:58 +0200

    various updates on the flow in level 1

--- Assets/Scripts/Quest/ManipulateStateTrigger.cs
+++ Assets/Scripts/Quest/ManipulateStateTrigger.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using StateMachine;
+using Character;
+
+
 
 public class ManipulateStateTrigger : MonoBehaviour
 {
@@ -8,7 +14,27 @@ public class ManipulateStateTrigger : MonoBehaviour
     bool triggered;
     LevelQuestManager levelquests;
     public LevelQuestManager.QuestProgress questProgress;
-    bool isNpc;
+    [SerializeField]bool isNpc;
+
+    public Transform IconCanvas;
+    public Sprite talk;
+    public Sprite objective;
+    public Collider perception;
+    public int degrees;
+    public float talkSize;
+    public float objectiveSize;
+    [HideInInspector]
+    public bool isActive = false;
+
+    [HideInInspector]
+    public Color alphaZero;
+    [HideInInspector]
+    public Color alphaMax;
+    [HideInInspector]
+    public Transform talkIcon;
+    [HideInInspector]
+    public Transform activePlayer;
+
 
     private void Start()
     {
@@ -21,7 +47,68 @@ public class ManipulateStateTrigger : MonoBehaviour
         {
             isNpc = transform.name.Contains("NpcQuest");
         }
+    }
 
+    private void Update()
+    {
+        talkIcon.DOLookAt(Camera.main.transform.position, 0.1f);
+        if(levelquests.actualQuest == questProgress -1 )
+        {
+
+        }
+    }
+
+    public void HideIcons()
+    {
+        talkIcon.GetChild(0).GetComponent<Image>().color = alphaZero;
+        talkIcon.GetChild(0).GetComponent<Image>().sprite = null;
+    }
+
+    public void SetToObjective()
+    {
+        talkIcon.GetChild(0).GetComponent<Image>().color = alphaMax;
+        talkIcon.GetChild(0).GetComponent<Image>().sprite = objective;
+        talkIcon.GetChild(0).GetComponent<Image>().rectTransform.sizeDelta = new Vector2(objectiveSize, objectiveSize);
+    }
+
+    public void SwapIcons(Transform orientation, CharacterStateController playerState)
+    {
+        orientation.GetChild(0).GetComponent<Image>().sprite = talk;
+        orientation.GetChild(0).GetComponent<Image>().color = alphaMax;
+        talkIcon.GetChild(0).GetComponent<Image>().rectTransform.sizeDelta = new Vector2(talkSize, talkSize);
+    }
+
+    public void ShowIcon(GameObject player)
+    {
+        CharacterStateController playerState = player.GetComponent<CharacterStateController>();
+
+        if (GMController.instance.isCharacterPlaying == playerState.thisCharacter)
+        {
+            activePlayer = player.GetComponent<_CharacterController>().m_Camera;
+            // Npc icon                       
+            if (playerState.thisCharacter == CharacterActive.Mother && !playerState.m_CharacterController.isPushDirectionRight ||
+                    playerState.thisCharacter == CharacterActive.Boy && !playerState.m_CharacterController.isClimbDirectionRight)
+            {
+                if (playerState.m_CharacterController.npcSightCollider.transform == perception.transform)
+                {
+                    SwapIcons(talkIcon, playerState);
+                    player.GetComponent<_CharacterController>().IconPriority(talkIcon, degrees);
+                    //talkIcon.DOLookAt(activePlayer.position, 0.1f);
+                }
+            }
+
+            else
+            {
+                if (isActive)
+                {
+                    SetToObjective();
+                }
+                else
+                {
+                    HideIcons();
+                }
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)

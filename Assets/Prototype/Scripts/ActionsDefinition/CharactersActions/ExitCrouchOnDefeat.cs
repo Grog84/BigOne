48a54787fc870fae14d910ea062c250d7842d2d2
Andrea Voscilla <Andrea.voscilla@hotmail.it>
2018-04-10 15:00:01 +0200

    fixed crouch post defeat

---
+++ Assets/Prototype/Scripts/ActionsDefinition/CharactersActions/ExitCrouchOnDefeat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using StateMachine;
+
+namespace Character.Actions
+{
+    [CreateAssetMenu(menuName = "Prototype/Actions/Characters/ExitCrouchOnDefeat")]
+    public class ExitCrouchOnDefeat : _Action
+    {
+        public override void Execute(CharacterStateController controller)
+        {
+            DetachChild(controller);
+        }
+
+        private void DetachChild(CharacterStateController controller)
+        {
+            controller.m_CharacterController.Crouch = false;
+        }
+    }
+}
